71282700b6e5aa709dc5513573dd6e56ae71acb7
CHODANIEL <eksldpf2299@naver.com>
2025-10-02 18:46:35 +0900

    4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
+import './App.scss'
+import Authpanel from './components/Authpanel.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path='/admin/login' element={<Authpanel />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/Authpanel.jsx
@@ -0,0 +1,8 @@
+// src/components/Authpanel.jsx
+export default function Authpanel() {
+    return (
+        <div>
+            로그인 패널
+        </div>
+    );
+}
